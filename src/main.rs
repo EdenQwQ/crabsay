9cbe111f9a65cc7fd29a27fc2397e1164c70cd22
EdenQwQ <lsahlm1eden@gmail.com>
2022-02-19 22:36:14 +0800

    [update] text from option to arg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, arg};
+use clap::{arg, App};
 
 fn main() {
     let args = args().get_matches();
@@ -7,10 +7,11 @@ fn main() {
     let think: bool = args.is_present("think");
     let eyes: String = args.value_of("eyes").unwrap().to_string();
     let mouth: String = args.value_of("mouth").unwrap().to_string();
+    let seperator: char = args.value_of("seperator").unwrap().chars().next().unwrap();
 
     let mut output: Vec<String> = Vec::new();
 
-    let lines = text.split('\n').collect::<Vec<&str>>();
+    let lines = text.split(seperator).collect::<Vec<&str>>();
     let mut length = 0;
     for line in &lines {
         if line.len() > length {
@@ -18,33 +19,42 @@ fn main() {
         }
     }
 
-    output.push("-".repeat(length));
+    output.push("_".repeat(length + 2));
+    output.push("".to_string());
     for line in lines {
         // output each line in the center of < > and with the correct length
-        output.push(format!("<{: ^1$}>", line, length));
+        output.push(format!(" {: ^1$} ", line, length));
     }
-    output.push("_".repeat(length));
+    output.push("_".repeat(length + 2));
 
     if think {
-        output.push("        o
+        output.push(
+            "        o
          o
-          o".to_string());
+          o"
+            .to_string(),
+        );
     } else {
-        output.push("        \\
+        output.push(
+            "        \\
          \\
-          \\".to_string());
+          \\"
+            .to_string(),
+        );
     }
 
-    output.push(format!("            _~^~^~_
+    output.push(format!(
+        "            _~^~^~_
         \\) /  {}  \\ (/
           '_   {}   _'
           / '-----' \\
-", eyes, mouth));
+",
+        eyes, mouth
+    ));
 
     for line in output {
         println!("{}", line);
     }
-
 }
 
 fn args() -> App<'static> {
@@ -52,32 +62,32 @@ fn args() -> App<'static> {
         .version("0.1.0")
         .author("EdenQwQ <lsahlm1eden@gmail.com>")
         .about("Cowsay rewriten in Rust but with Ferris")
+        .arg(arg!([text] "Text to say/think"))
         .arg(
-            arg!(-t --text <TEXT> "Text to be said")
-                .required(false)
-                .takes_value(true)
-                .help("Text to be said")
-                .default_value("Hello World!"),
-        )
-        .arg(
-            arg!(-t --think)
+            arg!(-t - -think)
                 .required(false)
                 .takes_value(false)
-                .help("If set, ferris will think rather than say.")
-                .default_value("false")
+                .help("If set, ferris will think rather than say."),
         )
         .arg(
-            arg!(-e --eyes)
+            arg!(-e - -eyes)
                 .required(false)
                 .takes_value(true)
                 .help("Sets the eyes of the ferris")
-                .default_value("o o")
+                .default_value("o o"),
         )
         .arg(
-            arg!(-m --mouth)
+            arg!(-m - -mouth)
                 .required(false)
                 .takes_value(true)
                 .help("Sets the mouth of the ferris")
-                .default_value("-")
+                .default_value("-"),
+        )
+        .arg(
+            arg!(-s - -seperator)
+                .required(false)
+                .takes_value(true)
+                .help("Sets the seperator of text lines.")
+                .default_value("\n"),
         )
 }
